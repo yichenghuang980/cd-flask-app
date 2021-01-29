bebba543354aff65f08dca2b102c4f30e8a30132
Wilson Huang <yh297@duke.edu>
2021-01-29 04:38:49 +0000

    Add cloudbuild.yaml file

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello I like to make AI Apps'
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
