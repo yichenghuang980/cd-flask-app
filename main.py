3eaad01bfe2e63cdc60802f0e4eee9c38efd7f41
Wilson Huang <yichenghuang980@gmail.com>
2021-02-01 22:18:59 +0000

    Add pandas and wikipedia function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import jsonify
+import pandas as pd
+import wikipedia
 
 app = Flask(__name__)
 
@@ -13,10 +15,24 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
-@app.route('/ab')
-def ab():
-    val = {"value": "ab"}
-    return jsonify(val)
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+    result = wikipedia.summary(company, sentences=10)
+    return result
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
