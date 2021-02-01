f3f6b4af385cc25ddc46c76dd57a16102185cb78
Wilson Huang <yichenghuang980@gmail.com>
2021-02-01 21:22:50 +0000

    push a build trigger

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
+@app.route('/bob')
+def bob():
+    val = {"value": "bob"}
+    return jsonify(val)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
